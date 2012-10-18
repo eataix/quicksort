cd1e7986b3b9fc6037840ab62b7013d33e4cb5ec
Meitian Huang <_@freeaddr.info>
2012-10-19 02:40:43 +1100

    Add FreeBSD Licence information.

--- distquicklib.c
+++ distquicklib.c
@@ -1,17 +1,41 @@
-/*-
+/**
  * distquicklib: library of concurrent and distributed quicksort
  * algorithms for COMP2310 Assignment 2, 2012.
  *
- * Name: StudentId:
+ * Name: Meitian Huang
+ *
+ * StudentId: u4700480
  *
  * ***Disclaimer***: (modify as appropriate) The work that I am submitting
  * for this program is without significant contributions from others
  * (excepting course staff).
  */
 
-// uncomment when debugging. Example call: PRINTF(("x=%d\n", x));
-// #define PRINTF(x) do { printf x; fflush(stdout); } while (0)
-#define PRINTF(x)               /* use when not debugging */
+/*-
+ * Copyright (c) 2012 Meitian Huang <_@freeaddr.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>             /* malloc, free */
